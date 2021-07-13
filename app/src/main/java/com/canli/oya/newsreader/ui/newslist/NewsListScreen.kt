03b5ee68acc9369406a22dcbeb5f412848929873
OyaCanli <oyacan@gmail.com>
2021-07-13 21:44:50 +0200

    use jetpack compose in details activity

--- app/src/main/java/com/canli/oya/newsreader/ui/newslist/NewsListScreen.kt
+++ app/src/main/java/com/canli/oya/newsreader/ui/newslist/NewsListScreen.kt
@@ -77,13 +77,13 @@ fun NewsItem(currentArticle: NewsArticle, position: Int, itemClickListener: List
             modifier = Modifier.fillMaxWidth()
         ) {
             ItemTitle(currentArticle.title, Modifier.weight(2f))
-            ItemImage(currentArticle.thumbnailUrl, Modifier.weight(1f))
+            ArticleImage(currentArticle.thumbnailUrl,
+                Modifier
+                    .weight(1f)
+                    .size(120.dp))
         }
         Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = currentArticle.articleTrail ?: "",
-            fontSize = 16.sp
-        )
+        TrailText(currentArticle.articleTrail)
         Spacer(modifier = Modifier.height(8.dp))
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -116,7 +116,17 @@ fun ItemTitle(title : String, modifier: Modifier) {
 }
 
 @Composable
-fun ItemImage(url : String?, modifier : Modifier) {
+fun TrailText(trailText : String?) {
+    trailText?.let {
+        Text(
+            text = it,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun ArticleImage(url : String?, modifier : Modifier) {
     Image(
         painter = rememberCoilPainter(
             url,
@@ -125,7 +135,6 @@ fun ItemImage(url : String?, modifier : Modifier) {
         contentDescription = "",
         contentScale = ContentScale.Crop,
         modifier = modifier
-            .size(120.dp)
     )
 }
 
@@ -151,7 +160,7 @@ fun Section(section : String) {
 }
 
 @Composable
-fun DateAndTime(date: String, modifier : Modifier) {
+fun DateAndTime(date: String, modifier : Modifier = Modifier) {
     Text(
         text = splitDateAndTime(date),
         modifier = modifier
@@ -193,7 +202,7 @@ fun ListPreview() {
         "politics/2021/jun/28/mps-criticise-ministers-failure-to-plan-industrial-policy",
         "MPs criticise ministers’ failure to plan industrial policy",
         "https://media.guim.co.uk/e1c5311fecbbcbf4c0267f48610f6fd30268b7f1/0_99_4000_2400/500.jpg",
-        "Jillian Ambrose, Jillian Ambrose, Jillian Ambrose and blabla",
+        "Jillian Ambrose",
         "Committee says firms have been left guessing with UK government’s ‘short-term approach’",
         "<p>The government’s failure to set out an industrial policy agenda has left UK businesses unclear about the future at a time when a green economic recovery is urgent, according to MPs on the business, energy and industrial strategy committee.</p> <p>The group of cross-party MPs has accused the government of taking a “short-termist, unclear and unwelcome” approach to the UK’s industrial policy since scrapping the business department’s industrial strategy in March.</p> <p>In a highly critical report the committee warned that the decision to abandon the strategy, put forward by Theresa May’s government in 2017, had led to <a href=\"https://www.theguardian.com/business/2021/mar/28/industrial-strategy-uk-build-back-better-covid-government\">a “fragmented” Treasury-led plan</a> which would risk “widening the gap” between government and business.</p> <p>Darren Jones, the chair of the committee, warned that the “short-termist” approach adopted by the prime minister and the chancellor came at a time when business leaders were “crying out for long-term consistency and clarity” as the UK began to rebuild a low-carbon, post-pandemic economy outside the EU.</p> <p>The government scrapped plans to overhaul its industrial strategy in favour of a Treasury-led “plan for growth” to help revive the economy in the wake of the Covid-19 crisis.</p> <p>“The reality is that it is nothing more than a list of existing policy commitments, many of which are hopelessly delayed,” Jones said. “Long term cross-economy challenges – from problems in productivity and our ageing workforce to the opportunities presented by new technologies and the net zero transition – no longer appear to command ministerial support as long-term, cross-party, whole-of-government policy priorities.”</p> <p>The committee added that the government’s decision to disband its in-house think-tank, the <a href=\"https://www.theguardian.com/business/nils-pratley-on-finance/2021/mar/23/why-the-uk-government-really-abolished-its-own-industrial-strategy-council\">Industrial Strategy Council</a>, led by the Bank of England’s Andy Haldane, would remove the “expert, independent oversight” which had offered “important guidance” on how to shape the UK’s industrial policies.</p>  <figure class=\"element element-embed\" data-alt=\"Sign up to the daily Business Today email\">  <iframe id=\"business-today\" name=\"business-today\" src=\"https://www.theguardian.com/email/form/plaintone/business-today\" scrolling=\"no\" seamless=\"\" class=\"iframed--overflow-hidden email-sub__iframe\" height=\"52px\" frameborder=\"0\" data-component=\"email-embed--business-today\"></iframe> <figcaption>Sign up to the daily Business Today email or follow Guardian Business on Twitter at @BusinessDesk </figcaption> </figure>  <p>Jones said business leaders and parliamentarians were “now left guessing what the government’s approach is to industrial policy, with no expert oversight reporting on what ministers have actually been able to deliver”.</p> <p>In response a government spokesperson said that since the industrial strategy was published more than four years ago the government had legislated to end the UK’s contribution to climate crisis by becoming a net-zero carbon economy by 2050, and was continuing to fight the Covid-19 pandemic while forging a new path outside the EU.</p> <p>“That’s why it was right to change our approach too, with our new plan for growth setting out the opportunities we’ll seize across the UK to drive economic growth, create jobs and support British industry as we level up and build back better out of this pandemic,” the spokesperson said.</p>",
         "Jun 28, 2021T04:01",
